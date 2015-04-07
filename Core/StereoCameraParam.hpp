eb8c6f9edbf2be5224a78759b74fcfbef717d157
toshi <toshi.mana.plus1@gmail.com>
2015-04-07 21:44:44 +0900

    qStringとStringの変換処理を追加

--- Core/StereoCameraParam.hpp
+++ Core/StereoCameraParam.hpp
@@ -7,33 +7,63 @@ typedef std::shared_ptr<StereoCameraParam> SpStereoCameraParam;
 
 struct StereoCameraParam
 {
-	cv::Mat lCameraMatrix;
-	cv::Mat lDistCoeffs;
+#define DECLARATION(type,name,tag) type name; const std::string TAG_##name = #tag;
 
-	cv::Mat rCameraMatrix;
-	cv::Mat rDistCoeffs;
+	DECLARATION( cv::Size, lImageSize, imageSizeL );
+	DECLARATION( cv::Mat, lCameraMatrix, cameraMatrixL );
+	DECLARATION( cv::Mat, lDistCoeffs,   distCoeffsL );
 
-	cv::Mat R;
-	cv::Mat T;
+	DECLARATION( cv::Size, rImageSize, imageSizeR );
+	DECLARATION( cv::Mat, rCameraMatrix, cameraMatrixR );
+	DECLARATION( cv::Mat, rDistCoeffs,   distCoeffsR );
 
-	double err;
+	DECLARATION( cv::Mat, R, rot );
+	DECLARATION( cv::Mat, T, trans );
+
+	DECLARATION( double, err, reprojectError );
+
+#undef declaration
 
 	int write( const std::string& path ) const
 	{
 		cv::FileStorage fs( path, cv::FileStorage::WRITE );
 		if ( !fs.isOpened() ) return -1;
 
-		fs << "cameraMatrixL" << lCameraMatrix;
-		fs << "distCoeffsL" << lDistCoeffs;
+		fs << TAG_lImageSize    << lImageSize;
+		fs << TAG_lCameraMatrix << lCameraMatrix;
+		fs << TAG_lDistCoeffs   << lDistCoeffs;
 
-		fs << "cameraMatrixR" << rCameraMatrix;
-		fs << "distCoeffsR" << rDistCoeffs;
+		fs << TAG_rImageSize    << rImageSize;
+		fs << TAG_rCameraMatrix << rCameraMatrix;
+		fs << TAG_rDistCoeffs   << rDistCoeffs;
 
-		fs << "rot" << R;
-		fs << "trans" << T;
+		fs << TAG_R << R;
+		fs << TAG_T << T;
 
-		fs << "reprojectError" << err;
+		fs << TAG_err << err;
 
 		return 0;
 	}
+
+	int read( const std::string& path )
+	{
+		cv::FileStorage fs( path, cv::FileStorage::READ );
+		if ( !fs.isOpened() ) return -1;
+		
+		fs[TAG_lImageSize]    >> lImageSize;
+		fs[TAG_lCameraMatrix] >> lCameraMatrix;
+		fs[TAG_lDistCoeffs]   >> lDistCoeffs;
+
+		fs[TAG_rImageSize]    >> rImageSize;
+		fs[TAG_rCameraMatrix] >> rCameraMatrix;
+		fs[TAG_rDistCoeffs]   >> rDistCoeffs;
+
+		fs[TAG_R] >> R;
+		fs[TAG_T] >> T;
+
+		fs[TAG_err] >> err;
+
+		return 0;
+	}
+
 };
